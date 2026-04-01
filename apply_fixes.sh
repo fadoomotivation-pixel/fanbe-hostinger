7666af2513c5f5ac6350365d65f6d94b3fc3443f
Fanbe Group <fanbegroup@gmail.com>
2026-04-01 23:36:56 +0530

    update employee leads pages

---
+++ apply_fixes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Applying audit fixes to EditLead.jsx and useCRMData.js..."
+
+# Fix 1: EditLead - update handleSubmit to clear follow-up on terminal statuses
+# Fix 2: useCRMData - clear follow-up in addBookingLog
+
+echo "Done! Now commit and push."
